7698af70a9f65cfedf919044af4552c2f71bcbfe
GideonQuinton <gideonquinton@gmail.com>
2024-10-05 21:14:49 -0600

    userdata.py changes

--- backend/userdata.py
+++ backend/userdata.py
@@ -1,23 +1,30 @@
 from flask import Flask
+import os
+
 
 class user:
-    def __init__ (self, username, password, xp, level):
+    def __init__ (self, username, password, xp):
         self.username = username
         self.password = password
         self.xp = xp
       
 users = [] #
 
-def make_new_user(username, password, xp):
-    temp = user()
-    temp.username = username
-    temp.password = password
-    temp.xp = 0
+# def make_new_user(username, password, xp):
+#     temp = user(username, password, 0)
+#     users.append(temp)
 
-users.append(temp)
+amogus = user("amongus77", "sussybaka", "0")
+users.append(amogus)
 
-f = open("userdata.txt", "a")
-#f.write("")
+print(f"{os.getcwd()}{os.sep}userdata.txt")
+with open("userdata.txt", "w") as f:
+    f.write(amogus.username)
+    f.write(amogus.password)
+    f.write(amogus.xp)
+    
+
+print('check')
 
 #need to 
     
