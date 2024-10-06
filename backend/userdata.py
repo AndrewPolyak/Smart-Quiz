0d508e2285585904f82b86b0e8b23c7cc4ddeda1
GideonQuinton <gideonquinton@gmail.com>
2024-10-05 21:46:28 -0600

    logintodo

--- backend/userdata.py
+++ backend/userdata.py
@@ -1,36 +1,12 @@
-from flask import Flask
-import os
-
-
 class user:
     def __init__ (self, username, password, xp):
         self.username = username
         self.password = password
         self.xp = xp
-      
-users = [] #
 
-# def make_new_user(username, password, xp):
-#     temp = user(username, password, 0)
-#     users.append(temp)
-
-amogus = user("amongus77", "sussybaka", "0")
-users.append(amogus)
-
-
-with open("userdata.txt", "w") as f:
-    f.write("joner boner")
-    f.write(amogus.username)
-    f.write(amogus.password)
-    f.write(amogus.xp)
-
-    
-
-print('check')
-
-#need to 
-    
-
-
-
-
+def add_user(username, password):
+    temp = user(username, password, 0)
+    with open("userdata.txt", "w") as f:
+        f.write(temp)
+def login(username, password):
+    #TODO
