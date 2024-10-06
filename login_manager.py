d8f932c1ddf32a88ef6e45fe292e6cb17a20c6bb
AndrewPolyak <andrew.polyak23@gmail.com>
2024-10-05 22:03:44 -0600

    Main pages implemented with Flask

--- login_manager.py
+++ login_manager.py
@@ -4,15 +4,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
     return render_template('login.html')
+
+@app.route('/create_acct')
+def create_acct():
+    return render_template('create_acct.html')
 
 @app.route('/handle_login', methods=['POST'])
 def handle_login():
     username = request.form.get("username")
     password = request.form.get("password")
     if username and password:
-        return f"Thank you {username}. You submitted password: {password}"
+        None # do something
 
+@app.route('/handle_acct_create', methods=['POST'])
+def handle_acct_create():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if username and password:
+        None # do something
 
 if __name__=="__main__":
     app.run(debug=True)
