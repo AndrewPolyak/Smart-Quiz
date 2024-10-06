0ae06900b045d668bd4b5d3daa5ba965d034e9a6
AndrewPolyak <andrew.polyak23@gmail.com>
2024-10-06 00:30:12 -0600

    Full app navigation now possible

--- backend/userdata.py
+++ backend/userdata.py
@@ -1,15 +1,17 @@
-class user:
+class User:
     def __init__ (self, username, password, xp):
         self.username = username
         self.password = password
         self.xp = xp
-temp = user('amogus', 'sussy', "0")
-balls = temp.username + ' | ' + temp.password + ' | ' + temp.xp
+
 def add_user(username, password):
-    temp = user(username, password, "0")
+    temp = User(username, password, "0")
     with open("userdata.txt", "w") as f:
-        f.write(string)
+        f.write(f"{temp.username},{temp.password},{temp.xp}\n")
+
+    return True
+
 def login(username, password): #CHECK IF USERNAME/PASSWORD IS IN USERDATA.TXT, if it is return true/sign in
+    None
     #TODO
 
-print()
--- front_end_manager.py
+++ front_end_manager.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from backend.userdata import add_user
 
 app = Flask(__name__)
 
@@ -14,19 +15,24 @@ def login():
 def create_acct():
     return render_template('create_acct.html')
 
+@app.route('/quiz')
+def quiz():
+    return render_template("quiz.html")
+
 @app.route('/handle_login', methods=['POST'])
 def handle_login():
     username = request.form.get("username")
     password = request.form.get("password")
     if username and password:
-        None # do something
+        create_acct(username, password)
 
 @app.route('/handle_acct_create', methods=['POST'])
 def handle_acct_create():
     username = request.form.get("username")
     password = request.form.get("password")
     if username and password:
-        None # do something
+        add_user(username=username, password=password)
+        return redirect(url_for('quiz'))
 
 if __name__=="__main__":
     app.run(debug=True)
